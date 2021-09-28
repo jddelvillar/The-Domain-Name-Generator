42fbee7de65b0814e98a58bd7f7780f80532c90a
jddelvillar <delvillar2017.j@gmail.com>
2021-09-28 19:08:38 +0000

    file finish Domain generator

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+var pronoun = ['the', 'our', 'yours', 'mine'];
+var adj = ['great', 'big', 'small', 'faster'];
+var noun = ['jogger', 'racoon', 'tiger', 'dog'];
+var extention = ['.es','.co', '.eu', '.fx']
+
+function website(pronoun, adj, noun, extention) {
+    return pronoun + adj + noun + extention
+}
+
+
+
+for (var i = 0; i < pronoun.length; i++) {
+    for (var j = 0; j < adj.length; j++) {
+        for (var k = 0; k < noun.length; k++) {
+            for (var l = 0; l < extension.length; l++){
+            console.log(website(pronoun[i], adj[j], noun[k], extention[l]))
+            }
+        }
+    }
+}
+
+  //const websites = () => {return }
