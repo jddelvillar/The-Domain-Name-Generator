45d990967d04caa928843822f348d731466400bb
jddelvillar <delvillar2017.j@gmail.com>
2021-10-06 08:09:07 +0000

    domain-generator-code

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ function website(pronoun, adj, noun, extention) {
 for (var i = 0; i < pronoun.length; i++) {
     for (var j = 0; j < adj.length; j++) {
         for (var k = 0; k < noun.length; k++) {
-            for (var l = 0; l < extension.length; l++){
-            console.log(website(pronoun[i], adj[j], noun[k], extention[l]))
+            for (var l = 0; l < extention.length; l++){
+            console.log(website(pronoun[i], adj[j], noun[k], extention[l]));
             }
         }
     }
 }
 
-  //const websites = () => {return }
+  
